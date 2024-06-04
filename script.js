d6b896c5014ee5f61e31b3a72998c8e13fef41d9
edu54gonza <lalitho54gonza@gmail.com>
2024-06-04 16:43:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-function openGoogle() {
-    window.open('https://www.google.com', '_blank');
-}
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Promoción de la Aplicación</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Bienvenido a Nuestra Aplicación</h1>
+    </header>
+    <main>
+        <section id="about">
+            <h2>Sobre la Aplicación</h2>
+            <p>Descubre ScanNav: tu guía nocturna infalible. Escanea, navega y llega seguro. ¡Conquista la noche sin miedo! 🚀...</p>
+        </section>
+        <section id="video">
+            <h2>Video de la Aplicación</h2>
+            <video controls>
+                <source src="anuncio scannav.gif" type="video/mp4">
+                Tu navegador no soporta la reproducción de videos.
+            </video>
